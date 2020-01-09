5fad7f1716757d27c8733fd968b85a1b0324d34b
alina-vorobyova <alina.vorobyova.dev@gmail.com>
2020-01-09 14:05:41 +0400

    Deadline fix

--- src/app/components/add-to-do-items/add-to-do-items.component.ts
+++ src/app/components/add-to-do-items/add-to-do-items.component.ts
@@ -23,20 +23,28 @@ export class AddToDoItemsComponent implements OnInit {
   get title() { return this.toDoItemForm.get('title') as FormControl }
   get text() { return this.toDoItemForm.get('text') as FormControl }
   get deadline() { return this.toDoItemForm.get('deadline') as FormControl }
+  get time() { return this.toDoItemForm.get('time') as FormControl }
 
   ngOnInit() {
     this.id = +this.activatedRoute.snapshot.paramMap.get('listid');
     this.toDoItemForm = new FormGroup({
        title: new FormControl(null, [Validators.required, Validators.maxLength(200)]),
        text: new FormControl(null,  [Validators.maxLength(1000)]),
-       deadline: new FormControl(null, null)
+       deadline: new FormControl(null, null),
+       time: new FormControl(null, null)
     });
   }
 
   
   async onFormSubmit() {
+    let time = this.time.value.toString().split(":");
+    let datetime = new Date(this.deadline.value);
+    datetime.setHours(+time[0]);
+    datetime.setMinutes(+time[1] + datetime.getTimezoneOffset());
+    
     let item = this.toDoItemForm.value;
     item.toDoListId = this.id;
+    item.deadline = datetime;
     await this.toDoListService.createToDoItem(item);
     this.router.navigate(['/toDoLists']);
 }
